ff04a1090e3c8d5380ff952e52fc639d35c881bd
grzegomill <grzegomill@gmail.com>
2018-03-16 23:55:20 +0100

    update for trello

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   
-  const apiRoot = 'http://frozen-refuge-51539.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://frozen-refuge-51539.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://frozen-refuge-51539.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://frozen-refuge-51539.herokuapp.com/v1/trello/';
   
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
